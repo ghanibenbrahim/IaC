6aca30c9aa6032224753ce7b14e929539e48f741
akramou01-dev <a.ouardas@esi-sba.dz>
2023-04-09 13:21:33 +0000

    remove providers.tf file

--- providers.tf
+++ providers.tf
@@ -9,12 +9,12 @@ terraform {
 
 
 provider "flexibleengine" {
-  tenant_id = "tenat_id"
-  domain_name = "domain_name"
-  access_key = "access_key"
-  secret_key  = " secret_key"
-  auth_url    = ""
-  region      = ""
+  tenant_id = "7664ed44f8264e63b00024d34173b5c2"
+  domain_name = "OCB0003815"
+  access_key = "HNY2IZGXQPWKZJM160I5"
+  secret_key  = " 0XxqUJ1AlczPML2rvRPkeYlQMiABpKXMRQF9WAR6"
+  auth_url    = "https://iam.eu-west-0.prod-cloud-ocb.orange-business.com:443/v3"
+  region      = "eu-west-0"
 }
 
 
